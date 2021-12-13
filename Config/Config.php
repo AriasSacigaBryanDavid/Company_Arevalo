9142d17e6b1d72c5e13cb1d6bcb9f394bb8ccae1
AriasSacigaBryanDavid <bariassaciga@gmail.com>
2021-12-12 20:37:44 -0500

    Updated

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+    const base_url = "http://localhost/proyecto/Company_Arevalo/";
+    const host = "localhost";
+    const user = "root";
+    const pass = "";
+    const db = "company_arevalo_bd";
+    const charset = "charse=utf8";
+?>
